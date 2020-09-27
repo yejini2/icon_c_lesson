994d790d3feb93b36a68b4a71c29c72e844384bb
yejini2 <63247385+yejini2@users.noreply.github.com>
2020-09-27 20:10:34 +0900

    9/27 (연습) 예진

---
+++ yejin/class.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello, world!\n");
+
+
+	printf("%s\n", "Hello World!");
+	printf("%s %s\n","Hello World", "finally");
+
+	return 0;
+}
