4f126d695cb491613d2c9d998ab4dafced948aa0
Swizmae <63235410+Uchin9703@users.noreply.github.com>
2020-09-27 21:09:02 +0900

    09.27 연습 김정희

---
+++ junghee/class.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() 
+{
+    printf("%s %s\n","Hello, world!","finally!");
+   // printf("Hello, world!\n");
+   // printf("Hello, world!\n");
+
+    return 0;
+}
