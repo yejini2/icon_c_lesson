e2560416c627e5cc7b5d41e51cfa0dbc8880550e
jpark0505 <jpark0505@naver.com>
2020-10-01 03:12:43 +0900

    0927 과제 (재현)

---
+++ jaehyeon/0927_misson_1.cpp
@@ -0,0 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	printf("%s\n%s", "아이콘 만세", "아이콘 만세");
+	return 0;
+}
---
+++ jaehyeon/0927_misson_2.cpp
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	printf("%s\n%s\n%s\n%s", "\\    /\\"," )  ( ')","(  /  )"," \\(__)|");
+
+	return 0; 
+}
