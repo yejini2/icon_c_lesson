5a6614a86e4bca5dd19655efa86b1256eb9e7e0f
yejini2 <63247385+yejini2@users.noreply.github.com>
2020-10-03 00:50:45 +0900

    9/27 미션 (예진)

---
+++ yejin/misson1.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+//9/27 mission
+//이예진
+
+
+
+
+int main() {
+
+
+	//1번 과제
+	//아이콘 수업이 시작되었습니다. 만세를 2번 외쳐볼까요?
+
+	printf("아이콘 만세\n");
+	printf("아이콘 만세\n");
+
+
+
+	printf("\n");
+	//2번 과제
+	//아래 예제와 같이 고양이를 출력하시오.
+	
+	printf("/    //\n");
+	printf(" )  ( ')\n");
+	printf("(  /  )\n");
+	printf("\(__)|\n");
+
+
+
+
+	return 0;
+
+
+}
+
