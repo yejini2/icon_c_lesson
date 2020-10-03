450926ff15ecc3694ddc3ffa950b183bc9580aa1
kimchaewon215 <203253@jnu.ac.kr>
2020-10-03 16:30:06 +0900

    10/3 homework no.1 chaewon

---
+++ chaewon/9월27일_2번과제.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+//2¹ø °úÁ¦
+int main() {
+
+	printf("%s\n", "\\    /\\");
+	printf("%s\n", " )  ( ')");
+	printf("%s\n", "(  /  )");
+	printf("%s\n", " \\(__)|");
+	return 0;
+}
