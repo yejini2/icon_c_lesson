3c4987c2ac4c43717fc7da5b906065b521bffdd3
kimchaewon215 <203253@jnu.ac.kr>
2020-10-03 17:23:09 +0900

    10/3 0927mission chaewon

---
+++ chaewon/0927_mission_1.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+// 0927_mission_1.cpp
+
+int main() {
+	printf("%s\n", "아이콘 만세");
+	printf("아이콘 만세");
+	return 0;
+}
---
+++ chaewon/0927_mission_2.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+// 0927_mission_2.cpp
+
+int main() {
+
+	printf("%s\n", "\\    /\\");
+	printf("%s\n", " )  ( ')");
+	printf("%s\n", "(  /  )");
+	printf("%s\n", " \\(__)|");
+	return 0;
+}
